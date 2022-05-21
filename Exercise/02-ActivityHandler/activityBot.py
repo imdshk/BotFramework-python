18d8d2dd44a7c4ded5abb77fd16793bd9527d28f
imdshk <83509028+imdshk@users.noreply.github.com>
2022-05-21 11:59:00 +0300

    Get memebers in the chat

--- Exercise/02-ActivityHandler/activityBot.py
+++ Exercise/02-ActivityHandler/activityBot.py
@@ -5,4 +5,6 @@ class ActivityBot(ActivityHandler):
     async def on_message_activity(self, turn_context: TurnContext):
         await turn_context.send_activity(turn_context.activity.text)
     async def on_members_added_activity(self, member_added: ChannelAccount, turnContext: TurnContext):
-        await turnContext.send_activity("Hello! Welcome to the Echo Bot!")
+        await turnContext.send_activity("Welcome! This is Activity Bot")
+        for member in member_added:
+            await turnContext.send_activity(member.name)
